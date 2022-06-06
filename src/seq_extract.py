480ae5c4e44b261c4e928830635452b3cfdf66f7
Ian Gordon <ianbgordon@gmail.com>
2022-06-06 16:54:18 -0400

    polish sequence extraction

--- src/seq_extract.py
+++ src/seq_extract.py
@@ -14,22 +14,28 @@ def uniform_extract(sequence, min_length=None, max_length=None):
     """
     min_length = 0 if min_length is None else min_length
     max_length = len(sequence) if max_length is None else max_length
-    seq_length = random.randint(min_length, max_length)
-    index = random.randint(min_length, len(sequence) - seq_length)
-    subsequence = sequence[index: index + seq_length]
+    assert 0 <= min_length <= max_length <= len(sequence)
+
+    subseq_length = random.randint(min_length, max_length)
+    index = random.randint(min_length, len(sequence) - subseq_length)
+    subsequence = sequence[index: index + subseq_length]
+
     return subsequence
 
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument("sequence", type=str)
-    parser.add_argument("--min-length", type=int)
-    parser.add_argument("--max-length", type=int)
+    parser.add_argument("sequence", type=str,
+                        help="The sequence from which we are extracting a subsequence")
+    parser.add_argument("--min-length", type=int,
+                        help="The minimum length of the extracted subsequence")
+    parser.add_argument("--max-length", type=int,
+                        help="The maximum length of the extracted subsequence")
     args = parser.parse_args(sys.argv[1:])
 
-    seq = uniform_extract(args.sequence, args.min_length, args.max_length)
+    subseq = uniform_extract(args.sequence, args.min_length, args.max_length)
 
-    print(seq)
+    print(subseq)
 
 
 if __name__ == "__main__":
